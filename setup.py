58ae21d62c8ff419ce772c0551b52b761a6a043f
Jimmy Jia <tesrin@gmail.com>
2015-09-04 20:59:55 -0400

    Add package information

--- setup.py
+++ setup.py
@@ -3,6 +3,24 @@ from setuptools import setup
 setup(
     name="Flask-JSONAPIView",
     version='0.0.3',
+    description="Building blocks for JSON API endpoints for Flask",
+    url='https://github.com/4Catalyzer/flask-jsonapiview',
+    author="Jimmy Jia",
+    author_email='tesrin@gmail.com',
+    license='MIT',
+    classifiers=(
+        'Development Status :: 2 - Pre-Alpha',
+        'Framework :: Flask',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ),
+    keywords='rest json api flask',
     packages=('flask_jsonapiview',),
     install_requires=(
         'Flask',
