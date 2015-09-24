a70635f98c75e17ad5b2f0202976adaf769288e1
Jimmy Jia <tesrin@gmail.com>
2015-09-23 21:48:33 -0400

    Add basic sorting class

---
+++ flask_jsonapiview/sorting.py
@@ -0,0 +1,57 @@
+__all__ = ('FixedSorting',)
+
+# -----------------------------------------------------------------------------
+
+
+class SortingBase(object):
+    def get_field_specs(self, fields):
+        if not fields:
+            raise ValueError("fields must not be empty")
+
+        return tuple(
+            self.get_field_spec(field) for field in fields.split(',')
+        )
+
+    def get_field_spec(self, field):
+        if not field:
+            raise ValueError("field must not be empty")
+
+        if field[0] == '-':
+            return field[1:], False
+
+        return field, True
+
+    def sort_query(self, query, view, field_specs):
+        criteria = self.get_criteria(view, field_specs)
+        return query.order_by(*criteria)
+
+    def get_criteria(self, view, field_specs):
+        return tuple(
+            self.get_criterion(view, field_spec) for field_spec in field_specs
+        )
+
+    def get_criterion(self, view, field_spec):
+        field_name, asc = field_spec
+        column = self.get_column(view, field_name)
+        return column if asc else column.desc()
+
+    def get_column(self, view, field_name):
+        self.validate_field_name(view, field_name)
+        return getattr(view.model, field_name)
+
+    def validate_field_name(self, view, field_name):
+        raise NotImplementedError()
+
+
+# -----------------------------------------------------------------------------
+
+
+class FixedSorting(SortingBase):
+    def __init__(self, fields):
+        self._field_specs = self.get_field_specs(fields)
+
+    def __call__(self, query, view):
+        return self.sort_query(query, view, self._field_specs)
+
+    def validate_field_name(self, field_name, view):
+        return True
