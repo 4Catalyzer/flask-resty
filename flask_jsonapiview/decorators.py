da7b83a35050e7194f901e3d374d1b3b7f233582
Jimmy Jia <tesrin@gmail.com>
2015-10-21 14:52:59 -0400

    Make decorators more flexible

--- flask_jsonapiview/decorators.py
+++ flask_jsonapiview/decorators.py
@@ -3,10 +3,16 @@ import functools
 # -----------------------------------------------------------------------------
 
 
-def get_item_or_404(method):
+def get_item_or_404(method=None, **decorator_kwargs):
+    # Allow using this as either a decorator or a decorator factory.
+    if method is None:
+        return functools.partial(get_item_or_404, **decorator_kwargs)
+
     @functools.wraps(method)
     def wrapped(self, *args, **kwargs):
-        item = self.get_item_or_404(kwargs.pop(self.url_id_key))
+        item = self.get_item_or_404(
+            kwargs.pop(self.url_id_key), **decorator_kwargs
+        )
         return method(self, item, *args, **kwargs)
 
     return wrapped
