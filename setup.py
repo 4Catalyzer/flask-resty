ae4383d59b6d2962f52e26094adf7cdedc126261
Jimmy Jia <tesrin@gmail.com>
2015-10-21 18:52:53 -0400

    0.0.17

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="Flask-JSONAPIView",
-    version='0.0.16',
+    version='0.0.17',
     description="Building blocks for JSON API endpoints for Flask",
     url='https://github.com/4Catalyzer/flask-jsonapiview',
     author="Jimmy Jia",
