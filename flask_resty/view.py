a8a434b1af1d186ed17ff004c9800b1a161225d7
Alex Rothberg <agrothberg@gmail.com>
2016-06-10 12:00:30 -0400

    Explicit use view model in get_item

--- flask_resty/view.py
+++ flask_resty/view.py
@@ -1,5 +1,6 @@
 import flask
 from flask.views import MethodView
+from sqlalchemy import and_
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm.exc import NoResultFound
 from werkzeug.exceptions import NotFound
@@ -185,7 +186,9 @@ class ModelView(ApiView):
     def get_item(self, id, create_missing=False):
         try:
             # Can't use self.query.get(), because query might be filtered.
-            item = self.query.filter_by(**self.get_id_dict(id)).one()
+            clauses = [getattr(self.model, k) == v
+                       for k, v in self.get_id_dict(id).iteritems()]
+            item = self.query.filter(and_(*clauses)).one()
         except NoResultFound:
             if create_missing:
                 item = self.create_missing_item(id)
