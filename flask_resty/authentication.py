8dd3409ca7d7fb4da5a721f939f83b232cde18b2
Jimmy Jia <tesrin@gmail.com>
2016-08-18 19:37:51 -0400

    Use app context directly instead of flask.g
    This is the official recommendation for libraries.

--- flask_resty/authentication.py
+++ flask_resty/authentication.py
@@ -1,14 +1,18 @@
-import flask
+from . import context
+
+# -----------------------------------------------------------------------------
+
+REQUEST_CREDENTIALS_KEY = 'request_credentials'
 
 # -----------------------------------------------------------------------------
 
 
 def get_request_credentials():
-    return getattr(flask.g, 'resty_request_credentials', None)
+    return context.get_context_value(REQUEST_CREDENTIALS_KEY, None)
 
 
 def set_request_credentials(credentials):
-    flask.g.resty_request_credentials = credentials
+    context.set_context_value(REQUEST_CREDENTIALS_KEY, credentials)
 
 
 # -----------------------------------------------------------------------------
