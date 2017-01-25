12247043eb6e5e21ccc582aaf276a0d5cd4fa927
Jimmy Jia <tesrin@gmail.com>
2017-01-25 17:35:13 -0500

    0.11.3

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ def system(command):
 
 setup(
     name="Flask-RESTy",
-    version='0.11.2',
+    version='0.11.3',
     description="Building blocks for REST APIs for Flask",
     url='https://github.com/4Catalyzer/flask-resty',
     author="Jimmy Jia",
