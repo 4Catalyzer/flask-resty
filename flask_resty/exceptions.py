018895c2148fe36458e02083968286949cd17790
Robert C Jensen <robert.cole.jensen@gmail.com>
2018-06-10 19:19:03 -0400

    add documentation for ApiError

--- flask_resty/exceptions.py
+++ flask_resty/exceptions.py
@@ -6,6 +6,16 @@ import flask
 
 
 class ApiError(Exception):
+    """An API exception. When raised, Flask-RESTy will send an HTTP response with
+    the provided `status_code`. The JSON encoded body contains a list of the
+    provided `errors` nested under a key named ``errors``.
+
+    If :py:attr:`flask.Flask.debug` or :py:attr:`flask.Flask.testing` is True,
+    the body will also contain the full traceback under a key named ``debug``.
+
+    :param int status_code: The HTTP status code for the error response
+    :param list errors: A list of dict with error metadata
+    """
     def __init__(self, status_code, *errors):
         self.status_code = status_code
         self.body = {
@@ -16,6 +26,13 @@ class ApiError(Exception):
             self.body['debug'] = traceback.format_exc()
 
     def update(self, additional):
+        """Add additional metadata to each error. Can be chained with
+        further updates.
+
+        :param dict additional: The additional metadata
+        :return: The :py:class:`ApiError` that :py:meth:`update` was called on
+        :rtype: :py:class:`ApiError`
+        """
         for error in self.body['errors']:
             error.update(additional)
 
