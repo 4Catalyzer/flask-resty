1f1875b0cbef50a7ee7ea5899f18d2469b54df1d
Robert C Jensen <robert.cole.jensen@gmail.com>
2018-06-09 20:15:29 -0400

    add authentication docs

--- flask_resty/authentication.py
+++ flask_resty/authentication.py
@@ -19,10 +19,25 @@ def set_request_credentials(credentials):
 
 
 class AuthenticationBase(object):
+    """Base class for the API authentication scheme. Flask-RESTy provides an
+    implementation using JSON Web Tokens but you can use any authentication
+    scheme by extending AuthenticationBase and implementing
+    get_request_credentials.
+    """
     def authenticate_request(self):
+        """Stores the request credentials in the Flask AppContext. No
+        validation is performed by Flask-RESTy. It is up to the implementor
+        to validate the request in get_request_credentials.
+        """
         set_request_credentials(self.get_request_credentials())
 
     def get_request_credentials(self):
+        """Retrieve the credentials from the current request. Typically this
+        is done by inspecting flask.request.
+
+        Implementing classes should raise a 401 Unauthorized ApiError on
+        authentication failure.
+        """
         raise NotImplementedError()
 
 
@@ -30,5 +45,7 @@ class AuthenticationBase(object):
 
 
 class NoOpAuthentication(object):
+    """An authentication scheme that always allows the request.
+    """
     def authenticate_request(self):
         pass
