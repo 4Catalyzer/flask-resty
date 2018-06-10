f24604adcbdcf32b2a04776f0bc1888bfcc33b4c
Robert C Jensen <robert.cole.jensen@gmail.com>
2018-06-10 18:56:12 -0400

    add get_item_or_404 docs
    FIXME: How to get an intersphinx link to type of <class function>

--- flask_resty/decorators.py
+++ flask_resty/decorators.py
@@ -4,6 +4,20 @@ import functools
 
 
 def get_item_or_404(func=None, **decorator_kwargs):
+    """
+    Similar to Django's :py:func:`django.shortcuts.get_object_or_404` but
+    as a decorator. Decorating :py:meth:`ApiView.retrieve`,
+    :py:meth:`ApiView.update` or :py:meth:`ApiView.destroy` replaces the `id`
+    parameter of the decorated method with the `item` corresponding to the
+    provided `id`.
+
+    Behaves like a decorator factory if `func` is omitted.
+
+    :param func: The function to decorate
+    :type func: function or None
+    :return: The decorated function or a decorator factory
+    :rtype: function
+    """
     # Allow using this as either a decorator or a decorator factory.
     if func is None:
         return functools.partial(get_item_or_404, **decorator_kwargs)
