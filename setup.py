6086c4611a4a52d9156004bf1ac7629936f09aa8
Jimmy Jia <tesrin@gmail.com>
2019-03-01 12:31:20 -0500

    feat: Mark test helpers as pytest plugin

--- setup.py
+++ setup.py
@@ -65,4 +65,7 @@ setup(
         'release': system('python setup.py clean package publish'),
         'test': system('tox'),
     },
+    entry_points={
+        "pytest11": ("flask-resty = flask_resty.testing",),
+    },
 )
