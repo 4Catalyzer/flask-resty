b959cfb8207adcd5c77343eb51cbf2779c5ed779
Steven Loria <sloria1@gmail.com>
2020-02-06 23:18:19 -0500

    fix: compatibility with werkzeug 1.0 (#304)
    * fix: compatibility with werkzeug 1.0
    
    close #303
    
    * Fix coverage

--- flask_resty/routing.py
+++ flask_resty/routing.py
@@ -1,4 +1,10 @@
-from werkzeug.routing import RequestSlash, Rule
+from werkzeug.routing import Rule
+
+try:
+    from werkzeug.routing import RequestPath
+except ImportError:  # pragma: no cover
+    # werkzeug<1.0
+    from werkzeug.routing import RequestSlash as RequestPath
 
 # -----------------------------------------------------------------------------
 
@@ -9,7 +15,7 @@ class StrictRule(Rule):
     def match(self, path, method=None):
         try:
             result = super().match(path, method)
-        except RequestSlash:
+        except RequestPath:
             return None
 
         return result
