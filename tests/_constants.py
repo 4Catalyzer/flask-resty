c95e30cbf5a157f119d58e9e1933e421af8dbd4b
Stephen Liu <sliu@quantum-si.com>
2025-04-25 14:46:21 -0400

    also update USE_LOAD_DEFAULT test attribute

--- tests/_constants.py
+++ tests/_constants.py
@@ -1,6 +1,9 @@
 import marshmallow
 
 # Field.missing is deprecated in favor of Field.load_default in marshmallow 3.13.0
-USE_LOAD_DEFAULT = marshmallow.__version_info__ >= (3, 13)
+try:
+    USE_LOAD_DEFAULT = marshmallow.__version_info__ >= (3, 13)
+except AttributeError:
+    USE_LOAD_DEFAULT = False
 
 LOAD_DEFAULT_KWARG = "load_default" if USE_LOAD_DEFAULT else "missing"
