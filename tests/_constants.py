27538f3dacf7910b1813d30ce59853b65e11b54c
Stephen Liu <sliu@quantum-si.com>
2025-09-09 12:04:29 -0400

    fix check for marshmallow version

--- tests/_constants.py
+++ tests/_constants.py
@@ -1,6 +1,11 @@
 import importlib.metadata
 
+import packaging.version
+
 # Field.missing is deprecated in favor of Field.load_default in marshmallow 3.13.0
-USE_LOAD_DEFAULT = importlib.metadata.version("marshmallow")
+_MARSHMALLOW_VERSION = importlib.metadata.version("marshmallow")
+USE_LOAD_DEFAULT = packaging.version.parse(
+    _MARSHMALLOW_VERSION
+) > packaging.version.parse("3.13")
 
 LOAD_DEFAULT_KWARG = "load_default" if USE_LOAD_DEFAULT else "missing"
