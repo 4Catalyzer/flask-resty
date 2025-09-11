cabed2788325974956cbb8055908373cb2283312
Stephen Liu <sliu@quantum-si.com>
2025-09-11 11:05:23 -0400

    remove superfluous marshmallow version check

--- tests/test_filtering.py
+++ tests/test_filtering.py
@@ -14,8 +14,6 @@ from flask_resty import (
 )
 from flask_resty.testing import assert_response
 
-from ._constants import LOAD_DEFAULT_KWARG
-
 # -----------------------------------------------------------------------------
 
 
@@ -105,7 +103,7 @@ def routes(app, models, schemas, filter_fields):
     class WidgetDefaultFiltersView(WidgetViewBase):
         filtering = Filtering(
             color=ModelFilter(
-                fields.String(**{LOAD_DEFAULT_KWARG: "red"}),
+                fields.String(load_default="red"),
                 lambda model, value: model.color == value,
             ),
             size=ColumnFilter(operator.eq, missing=1),
